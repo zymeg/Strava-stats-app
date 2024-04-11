ccb564c5f5f2420f2683f61974c005cb7aa51076
Szymon Jaskuła <sjaskula@lbpro.pl>
2024-04-11 10:15:40 +0200

    modified:   src/router/index.js

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 import UserProfile from '../views/UserProfile.vue'
 import App from '../App.vue'
 import OutOfLimit from '../views/OutOfLimit.vue'
+import Loader from '@/components/Loader.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.VITE_ENV_URL),
   routes: [
     {
-      path: '/*',
+      path: '',
       name: 'Main',
       componen: App
     },
@@ -20,6 +21,11 @@ const router = createRouter({
       path: '/strava-api-err-too-many-requests',
       name: 'OutOfLimit',
       component: OutOfLimit
+    },
+    {
+      path: '/exchange_token*',
+      name: 'exchange_token',
+      component: Loader
     }
   ]
 })
